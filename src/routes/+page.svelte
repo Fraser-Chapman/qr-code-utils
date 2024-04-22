11a145766010ea31b07a85784aeba1b95f36b420
Fraser Chapman <Fraser.Chapman@autotrader.co.uk>
2024-04-22 20:06:03 +0100

    First attempt at generating a QR code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,23 @@
+<script lang="ts">
+	import QRCode from 'qrcode';
+	let inputValue: string;
+
+
+	function generateCode(input: string): void {
+		QRCode.toDataURL(input, function (err, url) {
+			console.log(url);
+		})
+	}
+
+</script>
+
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
 		<h2 class="h2">Generate QR</h2>
+
+		<div id="container">
+			<input type="text" name="input" id="input" placeholder="url" bind:value={inputValue}>
+			<button on:click={generateCode(inputValue)}>Submit</button>
+		</div>
 	</div>
 </div>
