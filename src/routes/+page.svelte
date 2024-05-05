66a19632f4985fdbeea3eb76dcc340a70835738d
Fraser Chapman <Fraser.Chapman@autotrader.co.uk>
2024-05-06 00:30:11 +0100

    First pass at drawing QR code on screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 	import QRCode from 'qrcode';
+
 	let inputValue: string;
 
 	function generateCode(input: string): void {
-		QRCode.toDataURL(input, function (err, url) {
-			console.log(url);
-		})
+			const canvas = document.getElementById('canvas')
+
+			QRCode.toCanvas(canvas, input, function(error) {
+				if (error) {
+					console.log(error)
+				}
+			})
+
 	}
 
 </script>
@@ -14,8 +20,9 @@
 	<div class="space-y-10 text-center flex flex-col items-center">
 		<h2 class="h2">Generate QR</h2>
 
-		<div id="container">
+		<div id="container" class="flex flex-col max-w-96">
 			<input class="input" type="text" name="input" id="input" placeholder="url" bind:value={inputValue} on:change="{generateCode(inputValue)}">
+			<canvas id="canvas"></canvas>
 		</div>
 	</div>
 </div>
