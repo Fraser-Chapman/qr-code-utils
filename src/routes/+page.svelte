8bb5bba1be6a5329386f1292aea00181a4d97271
Fraser Chapman <Fraser.Chapman@autotrader.co.uk>
2024-05-06 00:51:30 +0100

    Add styling and replace usages of div with alternatives

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	let inputValue: string;
 
 	function generateCode(input: string): void {
-			const canvas = document.getElementById('canvas')
+			const canvas = document.getElementById('qr')
 
 			QRCode.toCanvas(canvas, input, function(error) {
 				if (error) {
@@ -16,13 +16,13 @@
 
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-10 text-center flex flex-col items-center">
+<main class="container h-full mx-auto flex justify-center items-center">
+	<section class="space-y-10 text-center flex flex-col items-center">
 		<h2 class="h2">Generate QR</h2>
 
-		<div id="container" class="flex flex-col max-w-96">
+		<section id="container" class="flex flex-col items-center content-between gap-7">
 			<input class="input" type="text" name="input" id="input" placeholder="url" bind:value={inputValue} on:change="{generateCode(inputValue)}">
-			<canvas id="canvas"></canvas>
-		</div>
-	</div>
-</div>
+			<canvas id="qr" width="100%" height="100%"></canvas>
+		</section>
+	</section>
+</main>
